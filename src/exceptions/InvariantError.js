b92fc1ffc2c65c2c1a4b5036b368447384e21c05
Lucky Ferdiansyah <luckyferdiansyah31@gmail.com>
2025-11-11 20:34:04 +0700

    postgres database implemented

--- src/exceptions/InvariantError.js
+++ src/exceptions/InvariantError.js
@@ -1,10 +1,10 @@
 const ClientError = require('./ClientError');
 
-class NotFoundError extends ClientError {
+class InvariantError extends ClientError {
   constructor(message) {
-    super(message, 404);
-    this.name = 'NotFoundError';
+    super(message);
+    this.name = 'InvariantError';
   }
 }
 
-module.exports = NotFoundError;
+module.exports = InvariantError;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -22,6 +24,38 @@ const init = async () => {
       validator: NotesValidator,
     },
   });
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // Jika error berasal dari ClientError (buatan sendiri)
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      // Jika error bukan server error (misal: 404)
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      // Jika error merupakan server error (500)
+      const newResponse = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    // Jika tidak ada error, lanjutkan seperti biasa
+    return h.continue;
+  });
+
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
